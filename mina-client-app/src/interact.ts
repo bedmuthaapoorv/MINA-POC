df2f3aa2747f89f86c2b5b26860893caadfc895a
bedmuthaapoorv <apoorvbedmutha@gmail.com>
2023-11-06 14:49:15 +0530

    added logging functionality

--- mina-client-app/src/interact.ts
+++ mina-client-app/src/interact.ts
@@ -8,10 +8,12 @@ import * as fs from "fs"
 
 const network=Mina.Network("https://proxy.berkeley.minaexplorer.com/graphql");
 Mina.setActiveInstance(network);
-
+const outputLog = fs.createWriteStream('./log.txt');
+const consoler = new console.Console(outputLog);
 if(!isRegistered()){
   // generate details and send them to LRA
-  console.log("registering");
+  //console.log("registering");
+  consoler.log("registering");
   // <-- how to generate different public keys for different users -->
   const userPrivateKey=PrivateKey.fromBase58("EKEPq8s8smnFkAhtoXETkDdb3m4jhugqzx82eYo5RNcJ3fiLryPF");
   const userPublicKey=PublicKey.fromPrivateKey(userPrivateKey);
@@ -31,9 +33,12 @@ if(!isRegistered()){
     "zkAppPublicKey": zkAppPublicKey.toBase58()
   }
   // send details to LRA for registration
-  console.log("sent details to verifier");
+  //console.log("sent details to verifier");
+  consoler.log("sent details to verifier");
+  
   let cert= await sendCreds(data);
   if(cert!=null){
+    //console.log("registration successful");
     console.log("registration successful");
     // store the certificate locally
     storeCert(cert);
@@ -45,11 +50,13 @@ if(!isRegistered()){
     // console.log(resp.toString());
 
     // compiles the smart contract code into a form that can be run on Mina runtime
-    console.log("compiling the certificate");
+    //console.log("compiling the certificate");
+    consoler.log("compiling the certificate");
     await Add.compile();
 
     let certObj=JSON.parse(cert);
-    console.log("building a storage request for blockchain")
+    // console.log("building a storage request for blockchain")
+    consoler.log("building a storage request for blockchain")
     const tx= await Mina.transaction({sender: userPublicKey, fee: 0.1e9},()=>{
       zkApp.update(Field(certObj["UUID"]), Field(certObj["token"]));
     });
@@ -61,22 +68,32 @@ if(!isRegistered()){
     // // the smart contract code is run locally before proving. 
     // // This is because the prover needs to know the output of the computation in order to generate the proof.
 
-    console.log("verifying the storage request for blockchain");
+    //console.log("verifying the storage request for blockchain");
+    consoler.log("verifying the storage request for blockchain");
+    
     await tx.prove();
 
     // every transaction needs to be signed by private keys else it will fail the verification stage
-    console.log("storing the certificate on blockchain");
+    // console.log("storing the certificate on blockchain");
+    consoler.log("storing the certificate on blockchain");
+    
     const sentTx=await tx.sign([userPrivateKey, zkAppPrivateKey]).send();
 
-    console.log("https://berkeley.minaexplorer.com/transaction/"+sentTx.hash());
+    //console.log("https://berkeley.minaexplorer.com/transaction/"+sentTx.hash());
+    consoler.log("https://berkeley.minaexplorer.com/transaction/"+sentTx.hash());
 
   }else{
-    console.log("registration unsuccessful");
+    //console.log("registration unsuccessful");
+    consoler.log("registration unsuccessful");
   }
 }else{
-  console.log("local certificate exists");
-  console.log("authenticating begins");
-  console.log("sending self signed certificate to verifier");
+  //console.log("local certificate exists");
+  //console.log("authenticating begins");
+  //console.log("sending self signed certificate to verifier");
+  consoler.log("local certificate exists");
+  consoler.log("authenticating begins");
+  consoler.log("sending self signed certificate to verifier");
+  
   // <-- sign the certificate -->
   const jsonString = fs.readFileSync('./src/Certificate.json', 'utf-8');
   const cert=JSON.parse(jsonString);
@@ -88,8 +105,10 @@ if(!isRegistered()){
   let authenticationResponse=await sendCreds(data);
   // console.log(authenticationResponse);
   if(authenticationResponse=="true"){
-    console.log("authentication successfull");
+    //console.log("authentication successfull");
+    consoler.log("authentication successfull");
   }else{
+    //console.log("authentication unsuccessfull");
     console.log("authentication unsuccessfull");
   }
 
--- smart-contract-component/src/API.ts
+++ smart-contract-component/src/API.ts
@@ -1,7 +1,7 @@
 import express, {json} from 'express' 
 import bodyParser from 'body-parser';
 import register, {authenticate} from './Register.js';
-
+import fs from 'fs'
 const app = express();
 
 app.use(json())
@@ -13,17 +13,23 @@ const PORT = process.env.PORT || 3000;
 //     res.json({ status: true, message: "Our node.js app works" })
 // });
 
+const outputLog = fs.createWriteStream('./log.txt');
+const consoler = new console.Console(outputLog);
+
 app.post('/authenticationMethods', async (req, res) => {
     let data = req.body;
     // res.send(JSON.stringify(data));
-    console.log(data["method"])
+    // console.log(data["method"])
+    consoler.log(data["method"])
      if(data["method"]===0){
-         console.log("registration begins at verifier");
-         res.send(await register(data));
+        // console.log("registration begins at verifier");
+        consoler.log("registration begins at verifier");
+        res.send(await register(data));
      }
      if(data["method"]===1){
-         console.log("authentication begins");
-         res.send(await authenticate(data));
+//        console.log("authentication begins");
+        consoler.log("authentication begins");
+        res.send(await authenticate(data));
      }
     //res.send(data)
 
